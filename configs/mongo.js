145e58d95d98542f6ef53ea5e66819044b4f7900
philip200720 <aposadas-2023357@kinal.edu.gt>
2025-03-04 17:16:12 -0600

    fixing bugs

--- configs/mongo.js
+++ configs/mongo.js
@@ -1,32 +1,33 @@
-'use strict'
+"use strict"
 
 import mongoose from "mongoose"
 
-export const dbConnection = async () => {
+export const dbConnection = async() => {
     try{
         mongoose.connection.on("error", () =>{
-            console.log("MongoDB | could not be connect to MongoDB")
+            console.log("MongoDB | could not connect to MongoDB")
             mongoose.disconnect()
         })
         mongoose.connection.on("connecting", () =>{
-            console.log("MongoDB | try connecting")
+            console.log("MongoDB | trying to connect")
         })
         mongoose.connection.on("connected", () =>{
             console.log("MongoDB | connected to MongoDB")
         })
         mongoose.connection.on("open", () =>{
-            console.log("MongoDB | Connnected to DataBase")
+            console.log("MongoDB | connected to database")
         })
         mongoose.connection.on("reconnected", () => {
             console.log("MongoDB | reconnected to MongoDB")
         })
         mongoose.connection.on("disconnected", () => {
-            console.log("MongoDB | disconnected to MongoDB")
+            console.log("MongoDB | disconnected from MongoDB")
         })
-        await mongoose.connect(process.env.URI_MONGO, {
+
+        await mongoose.connect(process.env.URI_MONGO,{
             serverSelectionTimeoutMS: 5000,
-            maxPoolSize: 50 
-        });
+            maxPoolSize: 50
+        })
     }catch(err){
         console.log(`Database connection failed: ${err}`)
     }
