e522c3a989a4bf2d470aa26641b2220330699773
philip200720 <aposadas-2023357@kinal.edu.gt>
2025-03-04 16:34:48 -0600

    updateUser logic modified

--- src/middlewares/user-validator.js
+++ src/middlewares/user-validator.js
@@ -15,7 +15,7 @@ export const registerValidator = [
     body("email").custom(emailExists),
     body("password").isStrongPassword({
         minLength: 8,
-        minLowercase:1,
+        minLowercase: 1,
         minUppercase: 1,
         minNumbers: 1,
         minSymbols: 1
@@ -29,7 +29,7 @@ export const registerValidator = [
 export const loginValidator = [
     body("email").optional().isEmail().withMessage("Not a valid email"),
     body("username").optional().isString().withMessage("Username is in the wrong format"),
-    body("password").isLength({min: 4}).withMessage("Password must contain at least 8 characters"),
+    body("password").isLength({ min: 4 }).withMessage("Password must contain at least 8 characters"),
     validateFields,
     handleErrors
 ]
@@ -43,8 +43,52 @@ export const listValidator = [
 
 export const updateUserValidator = [
     validateJWT,
-    hasRoles("ADMIN_ROLE","CLIENT_ROLE"),
-    body("uid").optional().isMongoId.withMessage("Not a valid MongoDB ID"),
+    hasRoles("ADMIN_ROLE", "CLIENT_ROLE"),
+    body("uid").optional().isMongoId().withMessage("Not a valid MongoDB ID"),
+    updateUserRoleValidation,
     validateFields,
     handleErrors
 ]
+
+const updateUserRoleValidation = async (req, res, next) => {
+    try {
+        const { usuario } = req
+        const role = usuario.role
+        const data = req.body
+
+        if (role === "ADMIN_ROLE" || data.uid) {
+            const user = await User.findById(data.uid)
+            if (user.role === "ADMIN_ROLE") {
+                return res.status(401).json({
+                    success: false,
+                    message: "Unable to edit other admins"
+                })
+            }
+            return next()
+        } else if (role === "CLIENT_ROLE") {
+            if (data.uid) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Only admins are authorized to edit other users"
+                })
+            } else if (data.role) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Only admins are authorized to edit roles"
+                })
+            } else if (data.status) {
+                return res.status(405).json({
+                    success: false,
+                    message: "Unable to switch status in update method"
+                })
+            }
+            return next()
+        }
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Error updating users",
+            error: err.message
+        })
+    }
+}
--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -23,52 +23,18 @@ export const getUsers = async (req, res) => {
 export const updateUser = async (req, res) => {
     try{
         const { usuario } = req
-        const role = usuario.role
         const data = req.body
 
-        if(role === "ADMIN_ROLE"){
-            const { uid } = req.body
-            const user = await User.findById(uid)
-            if(user.role === "ADMIN_ROLE"){
-                res.status(401).json({
-                    success: false,
-                    message: "Unable to edit other admins"
-                })
-            }else if(user.role === "CLIENT_ROLE"){
-                user = await User.findByIdAndUpdate(uid, data, { new: true})
-                res.status(200).json({
-                    success: true,
-                    message: "User updated successfully",
-                    user
-                })
-            }
-
-            res.status(200).json({
+        if(data.uid){
+            const user = await User.findByIdAndUpdate(uid, data, { new: true})
+            return res.status(200).json({
                 success: true,
                 message: "User updated successfully",
                 user
             })
-        }else if(role === "CLIENT_ROLE"){
-            if(data.uid){
-                res.status(401).json({
-                    success: false,
-                    message: "Only admins are authorized to edit other users"
-                })
-            }else if(data.role){
-                res.status(401).json({
-                    success: false,
-                    message: "Only admins are authorized to edit roles"
-                })
-            }else if(data.status){
-                res.status(405).json({
-                    success: false,
-                    message: "Unable to switch status in update method"
-                })
-            }
-            const { uid } = usuario
-            const user = await User.findByIdAndUpdate(uid, data, { new: true})
-
-            res.status(200).json({
+        }else{
+            const user = await User.findByIdAndUpdate(usuario.uid, data, { new: true})
+            return res.status(200).json({
                 success: true,
                 message: "User updated successfully",
                 user
